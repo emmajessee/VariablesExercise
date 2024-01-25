9f477524fb19ba09836565d33f4d0aa08c849151
Emma Jessee <emma.jessee@stu.glasgow.kyschools.us>
2024-01-25 10:23:06 -0600

    script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// 1. 
+// A constant's value will not change throughout coding in a file. 
+
+// 2. 
+let num = 20; 
+
+// 3.
+num = 15; 
+
+// 4. 
+num = num+=7; //22 
+
+// 5. 
+num = num - 1; //21
+
+// 6. 
+num = "This is not an number"
+console.log(num); //This is not a number 
+
+//7. 
+let blank; 
+
+//8. 
+console.log(blank); //undefined
+
+//9. 
+blank = null; // null
